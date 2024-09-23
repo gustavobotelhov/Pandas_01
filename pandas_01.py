3419ccfb04e9f2c6d7332823fd037afa811557f8
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-09-23 14:58:12 -0300

    adicionando exemplos de pd.series

---
+++ pandas_01.py
@@ -0,0 +1,42 @@
+# %% 
+import pandas as pd
+# %%
+idades = [10,18,30,29]
+media = sum(idades) / len(idades)
+
+total = 0
+
+for i in idades:
+    total += (media - i)**2
+
+variancia = total / (len(idades) - 1)
+# %%
+# definindo Series
+series_idades = pd.Series(idades)
+#%%
+# Media
+series_idades.mean()
+# %%
+# Mediana
+series_idades.median()
+# %%
+# Descrever
+series_idades.describe()
+#%%
+#Dimensão da série
+series_idades.shape
+#%% 
+# Variancia
+series_idades.var()
+#%%
+# Desvio padrao
+series_idades.std()
+ # %% 
+ # Verificar os index
+series_idades.index
+# %%
+#Alterando index da serie
+series_idades.index = ['t','o','a','x']
+#%%
+#Navegando nos indices novos
+series_idades['t']
